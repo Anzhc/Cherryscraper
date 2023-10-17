9d47226d0890b32304dc30cccf1795029fd38503
Anzhc <133806049+Anzhc@users.noreply.github.com>
2023-10-17 12:21:52 +0400

    Actually that was all old stuff, V2 update

--- CherryScraper/background.js
+++ CherryScraper/background.js
@@ -24,28 +24,56 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             let downloadDirectory = data.downloadDirectory ? data.downloadDirectory + '/' : '';
 
             // Download the tags.
-            chrome.downloads.download({url: request.tagsUrl, filename: downloadDirectory + request.tagsFilename}, function() {
+            chrome.downloads.download({url: request.tagsUrl, filename: downloadDirectory + request.tagsFilename}, function(downloadId) {
                 // Revoke the Data URL once the download has started.
                 URL.revokeObjectURL(request.tagsUrl);
+
+                // Listen for the onChanged event for this download.
+                chrome.downloads.onChanged.addListener(function(delta) {
+                    if (delta.id === downloadId && delta.state && delta.state.current === 'complete') {
+                        // The download has completed. Close the tab if automaticDownload is enabled.
+                        if (request.automaticDownload) {
+                            chrome.tabs.remove(request.tabId);
+                        }
+                    }
+                });
             });
         });
     }
+    else if (request.action === 'getTabId') {
+        sendResponse({tabId: sender.tab.id});
+    }
 });
+
 // Listen for when the active tab changes
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-    console.log('Tab activation event detected');  // New log statement
     // Get the URL of the new active tab
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-        console.log('Tab URL: ', tab.url);  // New log statement
         if (tab.url) {
-            chrome.storage.sync.get('siteList', function(data) {
-                console.log('Site list: ', data.siteList);  // New log statement
+            chrome.storage.sync.get(['siteList',  'automaticDownload'], function(data) {
                 let matchingSite = data.siteList.find(site => tab.url.includes(site));
-                if (matchingSite) {
-                    console.log('Matching site: ', matchingSite);  // New log statement
+                if (matchingSite && data.automaticDownload) {
                     chrome.storage.sync.set({selectedSite: matchingSite});
+
+                    // Send message to the content script to start scraping, include automaticDownload in the message.
+                    // chrome.tabs.sendMessage(activeInfo.tabId, {action: "gatherData", automaticDownload: data.automaticDownload});
                 }
             });
         }
     });
+});
+
+// Listen for when a tab is updated
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        chrome.storage.sync.get(['siteList', 'automaticDownload'], function(data) {
+            let matchingSite = data.siteList.find(site => tab.url.includes(site));
+            if (matchingSite && data.automaticDownload) {
+                chrome.storage.sync.set({selectedSite: matchingSite});
+
+                // Send message to the content script to start scraping, include automaticDownload in the message.
+                // chrome.tabs.sendMessage(tabId, {action: "gatherData", automaticDownload: data.automaticDownload});
+            }
+        });
+    }
 });
